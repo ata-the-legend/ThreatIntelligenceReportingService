b0656c2905456330d67c268c285f836d77a6ed62
Ata Safapour <safapour.ata@gmail.com>
2024-01-19 12:17:26 +0330

    connected to postgres & configed db

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://ata:1234@127.0.0.1:5432/ThreatIntel"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
---
+++ models.py
@@ -0,0 +1,23 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, TIMESTAMP
+from datetime import datetime
+
+
+class User(Base):
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+
+
+class SuspiciousIP(Base):
+    
+    __tablename__ = 'suspiciousIPs'
+
+    id = Column(Integer, primary_key=True, index=True)
+    ip_address = Column(String)
+    report_count = Column(Integer, server_default='0')
+    last_reported = Column(TIMESTAMP, default=datetime.now, onupdate=datetime.now)
